f16ff1564a66576e46d354b8a70fe8bbc07f70da
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-02-28 13:18:06 -0300

    Add update countdown time

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,8 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useState } from 'react';
 import Cookies from 'js-cookie';
 import challenges from '../../challenges.json';
 import { LevelUpModal } from '../components/LevelUpModal';
